07ca153bd5614178f01fb857de3d945d0e139f87
tangchengdong <592217679@qq.com>
2024-07-11 20:13:48 +0800

    add

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from user.views import RegisterView, LoginView, MeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('signup/', RegisterView.as_view(), name='signup'),
+    path('signin/', LoginView.as_view(), name= 'signin'),
+    path('me/', MeView.as_view(), name='me'),
 ]
